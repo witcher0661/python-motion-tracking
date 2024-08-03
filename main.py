be984dc89b42f8ccd6d7405951b9c13f2eed66ed
Jamie <Jamie.witcher@yahoo.co.uk>
2024-08-03 17:51:38 +0100

    this commit will add a save function

---
+++ main.py
@@ -0,0 +1,11 @@
+from utils import read_video, save_video
+
+def main():
+    # Read Video
+    video_frames = read_video(r"C:\Users\44780\Documents\python-motion-tracking\input_videos\08fd33_4.mp4")
+
+    # Save video
+    save_video(video_frames, r"C:\Users\44780\Documents\python-motion-tracking\output_videos\output.avi")
+
+if __name__ == '__main__':
+    main()
