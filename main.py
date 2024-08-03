270872208f9fa712e777dba8a63af7669c877738
Jamie <Jamie.witcher@yahoo.co.uk>
2024-08-03 21:28:50 +0100

    this commit will run the supervision tracker

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ def main():
     # Read Video
     video_frames = read_video(r"C:\Users\44780\Documents\python-motion-tracking\input_videos\08fd33_4.mp4")
 
+    # Initialize Tracker
+    tracker = Tracker('models/best.pt')
+
+    tracks = tracker.get_object_tracks(video_frames)
+
     # Save video
     save_video(video_frames, r"C:\Users\44780\Documents\python-motion-tracking\output_videos\output.avi")
 
