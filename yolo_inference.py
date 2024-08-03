f9d04dc32f5a99b6d71c386cd542dde6a82d5186
Jamie <Jamie.witcher@yahoo.co.uk>
2024-08-03 17:07:39 +0100

    this commit will run yolo models/best.pt

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO 
 
-model = YOLO("yolov8x")
+model = YOLO("models/best.pt")
 
 results = model.predict(r"C:\Users\44780\Documents\python-motion-tracking\input_videos\08fd33_4.mp4",save=True)
 print(results[0])
