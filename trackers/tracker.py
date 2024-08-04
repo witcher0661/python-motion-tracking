a4dcd757759edb296a7e49c4108f8dbd9dafe38a
Jamie <Jamie.witcher@yahoo.co.uk>
2024-08-04 21:44:36 +0100

    this tracker will add ellipse to players

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -2,6 +2,10 @@ from ultralytics import YOLO
 import supervision as sv
 import pickle
 import os
+import cv2
+import sys 
+sys.path.append('../')
+from utils import get_center_of_bbox, get_bbox_width
 
 class Tracker:
     def __init__(self, model_path):
@@ -74,6 +78,25 @@ class Tracker:
 
         return tracks
     
+    def draw_ellipse(self,frame,bbox,color,track_id=None):
+        y2 = int(bbox[3])
+        x_center, _ = get_center_of_bbox(bbox)
+        width = get_bbox_width(bbox)
+
+        cv2.ellipse(
+            frame,
+            center=(x_center,y2),
+            axes=(int(width), int(0.35*width)),
+            angle=0.0,
+            startAngle=-45,
+            endAngle=235,
+            color = color,
+            thickness=2,
+            lineType=cv2.LINE_4
+        )
+
+        return frame
+
 
     def draw_annotations(self,video_frames, tracks):
         output_video_frames= []
